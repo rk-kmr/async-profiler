9c14fe010f3e1ec411ad7c61ed4a74e1e7fb2d68
Rohitash Kumar <kurohita@amazon.com>
2025-07-21 11:54:37 +0100

    poc implementation to log process info with timer loop.

--- src/proc_recorder.h
+++ src/proc_recorder.h
@@ -7,40 +7,48 @@
 #define _PROCRECORDER_H
 
 #include <stdint.h>
+#include <pthread.h>
 #include "engine.h"
 #include "event.h"
 #include "mutex.h"
 #include "trap.h"
+#include "os.h"
 
 class ProcRecorder : public Engine {
   private:
-  /*
-    static u64 _interval;
-    static bool _nofree;
-    static volatile u64 _allocated_bytes;
-
-    static Mutex _patch_lock;
-    static int _patched_libs;
-    static bool _initialized;
-  */
     static volatile bool _running;
+    static int _interval;  // Collection interval in milliseconds
+
+    pthread_t _timer_thread;
+
+    void timerLoop();
+    void collectProcessMetrics();
+
+    static void* threadEntry(void* proc_recorder) {
+        ((ProcRecorder*)proc_recorder)->timerLoop();
+        return NULL;
+    }
 
   public:
     const char* type() {
-        return "Proc Recorder";
+        return "proc";
     }
 
     const char* title() {
-        return "Process Records";
+        return "Process Metrics";
     }
 
+    const char* units() {
+        return "processes";
+    }
+
+    Error check(Arguments& args);
     Error start(Arguments& args);
     void stop();
 
     static inline bool running() {
         return _running;
     }
-
 };
 
 #endif // _PROCRECORDER_H
